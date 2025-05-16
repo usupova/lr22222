6aec023363c697858a5cdbf9c6984abf4bae3940
usupova <157115365+usupova@users.noreply.github.com>
2025-05-16 20:05:18 +0300

    Add files via upload

---
+++ pr lr22222.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.a = 2;
+            rect.b = 3;
+            rect.ShowDetails();
+            double p = rect.P();
+            Console.WriteLine($"Периметр: {p}");
+            Console.ReadKey();
+        }
+    }
+}
